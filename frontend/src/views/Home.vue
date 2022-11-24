004adb26889eebf510a75a709d5bbc73dda757aa
Ruslan <31005007+ruslanee@users.noreply.github.com>
2022-11-24 15:36:54 +0200

    change acceptance criteria

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -7,19 +7,18 @@
       Ambulance teams - board for managing ambulance teams
 
       Doctors view acceptance criterias:
-      1) Able to see list of all doctors in a table format? Able to sort ASC/DESC each column. Able to  search by doc first/last name
+      1) able to see list of all doctors in a table format with pagination. Able to sort ASC/DESC each column. Able to search by doc first/last name
       2) able to add new doctor
       3) able to delete/ or make inactive
 
       Medical teams view acceptance criterias:
-      1) Able to see list of all teams in a table format? Able to sort ASC/DESC each column. Able to search by team name.
+      1) able to see list of all teams in a table format with pagination. Able to sort ASC/DESC each column. Able to search by team name.
       2) by clicking on a team able to see team details and assigned doctors
       3) able to add/remove doctors
       4) only active doctors can be added
 
       Use some styling library: Vuetify or Bootstrap-vie or Bulma - whatever you like
 
-      Setup Selenide tests (or vanilla Selenium) and make some test for every page.
     </pre>
   </div>
 
